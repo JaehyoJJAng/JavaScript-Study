a1f0fbc3393f66da6d6ab2c4e202ada1623e449c
JaehyoJJAng <yshrim12@naver.com>
2022-12-27 21:19:52 +0900

    (21) 배열과 반복문

---
+++ 00_파이썬_개발자를위한_JS기초/20_배열과_반복문/js/main.js
@@ -0,0 +1,14 @@
+let dataTypes = ['number', 'string', 'boolean', 'null', 'undefined', 'array'];
+
+// 여기에 코드를 작성하세요
+{
+  let forCount=0;
+  for (
+    forCount;
+    forCount < dataTypes.length;
+    forCount++
+    ) {
+      data = dataTypes[forCount];
+      console.log(data);
+  }
+}
---
+++ 00_파이썬_개발자를위한_JS기초/20_배열과_반복문/python/main.py
@@ -0,0 +1,4 @@
+data_types = ['number', 'string', 'boolean', 'null', 'undefined', 'array']
+
+for i in range(len(data_types)):
+    print(data_types[i])
