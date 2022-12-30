b4e8c8451a3a57a5f41389f9adca95c1ff228bf9
JaehyoJJAng <yshrim12@naver.com>
2022-12-30 20:57:33 +0900

    (40) [자료형] 숫자형

---
+++ 01_프로그래밍_핵심개념_in_자바스크립트/01_[자료형]_숫자형/js/main.js
@@ -0,0 +1,19 @@
+// 덧셈
+console.log(1 + 8);
+
+// 뺄셈
+console.log(6 - 7);
+
+// 곱셈
+console.log(2 * 2 * 2);
+
+// 거듭제곱
+console.log(2 ** 3);
+
+// 나눗셈
+console.log(5 / 2);
+
+// 나머지 연산
+console.log(7 % 3);
+
+console.log((2 + 3) * 2);
