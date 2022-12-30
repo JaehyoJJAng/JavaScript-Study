c7fadba126a27a160d273a043d7a8e62dd221275
JaehyoJJAng <yshrim12@naver.com>
2022-12-30 22:11:06 +0900

    (42) [자료형] 문자열 활용

---
+++ 01_프로그래밍_핵심개념_in_자바스크립트/03_[자료형]_문자열_활용/js/main.js
@@ -0,0 +1,18 @@
+// 문자열
+console.log('Hello I\'m Jaehyo Lee');
+
+// 벡틱 사용하기
+console.log(`Hello I'm Jaehyo Lee`)
+
+// 변수 활용하기
+var Name = "Jaehyo Lee";
+console.log(`Hello I'm ${Name}`);
+
+// 문자열 연산
+{
+  var Name = "Jaehyo Lee"
+  var yourName = "Doosik Lee"
+
+  console.log(`${Name} + ${yourName} = ${Name + yourName}`)
+  console.log(`3 + 5 = ${'3' + '5'}`) // 35
+} 
